9c7ec3d1c9e9a9706ce15c4e962bc5f21922eef7
kim gyu bhin <kim gyu bhin@DESKTOP-KP6JMD9>
2022-09-18 22:16:34 +0900

    hello world

--- src/kyubin/org/chap01/note01.java
+++ src/kyubin/org/chap01/note01.java
@@ -3,7 +3,7 @@ package kyubin.org.chap01;
 public class note01 {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!");
 	}
 	//과제 이외에 따로 정리할 때 사용
 }
