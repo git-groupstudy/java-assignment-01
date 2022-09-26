721b9c434d23aa5529cb3dce13e21fafc40722d9
kim gyu bhin <kim gyu bhin@DESKTOP-KP6JMD9>
2022-09-26 12:40:35 +0900

    Operator-int

---
+++ src/kyubin/assign/chap01/exam01/task2.java
@@ -0,0 +1,10 @@
+package kyubin.assign.chap01.exam01;
+
+public class task2 {
+
+	public static void main(String[] args) {
+		//1. 콘솔창에 Hello World를 출력해보세요.
+		System.out.println("Hello World");
+	}
+
+}
--- src/kyubin/org/chap01/note01.java
+++ src/kyubin/org/chap01/note01.java
@@ -3,7 +3,15 @@ package kyubin.org.chap01;
 public class note01 {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!!");
+		int a = 10;
+		int b = 5;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(b%a);
+		
 	}
 	//과제 이외에 따로 정리할 때 사용
 }
