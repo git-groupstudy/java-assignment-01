eb611f01fc9a61fc797929e54772a50080661102
KIM gyu bin <KIM gyu bin@DESKTOP-O20N9NF>
2022-10-03 15:31:49 +0900

    3차시 숙제 수정본

---
+++ src/kyubin/assign/chap03/exam01/Task03.java
@@ -0,0 +1,150 @@
+package kyubin.assign.chap03.exam01;
+
+public class Task03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1. for문을 사용해 1~100까지 자연수 중 짝수 만 출력하기
+		// 힌트) %연산자를 사용하면 홀수, 짝수 판별이 가능합니다.
+
+		for (int i = 0; i < 101; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+
+		// 2. 1번 문제를 %연산자를 사용하지 않고 출력해봅시다.
+		// 힌트) for(초기값; 조건문; 증가값)중 어떤 값을 바꿔야 할 지 잘 생각해봅시다.
+
+		for (int i = 0; i < 101; i += 2) {
+			System.out.println(i);
+		}
+
+		// 3. for문을 이용해 100->0 까지 1씩 작아지면서 출력되도록 만들어봅시다.
+		// 힌트) 2번 문제를 해결하셨다면 충분히 하실 수 있습니다:)
+
+		for (int i = 100; i >= 0; i--) {
+			System.out.println(i);
+		}
+
+		// 4. while문을 사용해 1~100까지 자연수 중 3의 배수의 합 구하기
+		// 힌트) %연산자 사용하여 3의 배수 찾기
+
+		int sum = 0;
+		int index = 0;
+
+		while (index < 100) {
+			if (index % 3 == 0) {
+				sum = index + sum;
+			}
+			index++;
+		}
+		System.out.println(sum);
+
+		// 5. 아래 그림과 같은 역삼각형을 출력해보자
+		/*
+		
+		  *******
+		   *****
+		    ***
+		     *
+		
+		*/
+		// 힌트) java-classes-01 프로젝트의 챕터03 Stars.java의 6번 피라미드응용을 참고해보면 됩니다. 어렵운걸 알지만
+		// 고민해보시면 좋을 것 같습니다.
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 7; j++) {
+				if (i > j) {
+					System.out.print(" ");
+				}
+
+			}
+			for (int j = 0; j < 7; j++) {
+				if (i <= (j / 2)) {
+					System.out.print("*");
+				}
+			}
+			System.out.println("");
+		}
+		// 6. 나이가 19세 이하이거나 60세 이상이면 할인해주려고 합니다.
+		// int age 값을 새로 초기화하여 검사할 때 할인 대상인지 아닌지 판별해봅시다.
+		// 아래 코드의 주석을 해제하고 () 안을 채워서 실행해보면 됩니다.
+
+		int age = 19;
+
+		if (age <= 19) {
+			System.out.println("할인 대상 O");
+		} else if (age >= 60) {
+			System.out.println("할인 대상 O)");
+		} else {
+			System.out.println("할인 대상 X");
+		}
+
+		// 7. if문을 사용하여 사용자가 정수를 입력하였을 때 그 값이 100보다 작은지 같은지 큰지를 판별해보자.
+		// 힌트) if - else if - else 문법을 사용
+
+		int numIf = 10;
+
+		if (numIf > 100) {
+			System.out.println(numIf + "는 100보다 큰 수입니다.");
+		} else if (numIf < 100) {
+			System.out.println(numIf + "는 100보다 작은 수입니다.");
+		} else {
+			System.out.println(numIf + "는 100입니다.");
+		}
+
+		// 8. switch문을 사용하여 A, B, C, D 학점이 존재할 때 score 값에 따라 해당 학점이 몇점인지 출력해주는 코드를
+		// 작성해봅시다.
+		// A = 4, B = 3, C = 2, D = 1
+		// 출력문 예시 : System.out.println("A학점은 4점입니다.");
+
+		String score = "A";
+
+		switch (score) {
+		case "A":
+			System.out.println("A학점은 4점입니다.");
+			break;
+		case "B":
+			System.out.println("B학점은 3점입니다.");
+			break;
+		case "C":
+			System.out.println("C학점은 2점입니다.");
+			break;
+		default:
+			System.out.println("D학점은 1점입니다.");
+			break;
+		}
+
+		// 9. 구구단을 아래와 같은 형태로 출력해봅시다.
+		/*
+		 * 2 X 1 = 02 | 3 X 1 = 03 | 4 X 1 = 04 | 5 X 1 = 05 | 6 X 1 = 06 | 7 X 1 = 07 |
+		 * 8 X 1 = 08 | 9 X 1 = 09 | 2 X 2 = 04 | 3 X 2 = 06 | 4 X 2 = 08 | 5 X 2 = 10 |
+		 * 6 X 2 = 12 | 7 X 2 = 14 | 8 X 2 = 16 | 9 X 2 = 18 | 2 X 3 = 06 | 3 X 3 = 09 |
+		 * 4 X 3 = 12 | 5 X 3 = 15 | 6 X 3 = 18 | 7 X 3 = 21 | 8 X 3 = 24 | 9 X 3 = 27 |
+		 * 2 X 4 = 08 | 3 X 4 = 12 | 4 X 4 = 16 | 5 X 4 = 20 | 6 X 4 = 24 | 7 X 4 = 28 |
+		 * 8 X 4 = 32 | 9 X 4 = 36 | 2 X 5 = 10 | 3 X 5 = 15 | 4 X 5 = 20 | 5 X 5 = 25 |
+		 * 6 X 5 = 30 | 7 X 5 = 35 | 8 X 5 = 40 | 9 X 5 = 45 | 2 X 6 = 12 | 3 X 6 = 18 |
+		 * 4 X 6 = 24 | 5 X 6 = 30 | 6 X 6 = 36 | 7 X 6 = 42 | 8 X 6 = 48 | 9 X 6 = 54 |
+		 * 2 X 7 = 14 | 3 X 7 = 21 | 4 X 7 = 28 | 5 X 7 = 35 | 6 X 7 = 42 | 7 X 7 = 49 |
+		 * 8 X 7 = 56 | 9 X 7 = 63 | 2 X 8 = 16 | 3 X 8 = 24 | 4 X 8 = 32 | 5 X 8 = 40 |
+		 * 6 X 8 = 48 | 7 X 8 = 56 | 8 X 8 = 64 | 9 X 8 = 72 | 2 X 9 = 18 | 3 X 9 = 27 |
+		 * 4 X 9 = 36 | 5 X 9 = 45 | 6 X 9 = 54 | 7 X 9 = 63 | 8 X 9 = 72 | 9 X 9 = 81 |
+		 */
+		// 힌트 : 2중 for문 사용,문자열 붙이기 사용, 조건문으로 한자리 수 00 포맷으로 출력
+
+		for (int i = 1; i < 10; i++) {
+			for (int j = 2; j < 10; j++) {
+				if (i * j < 10) {
+					System.out.print(j + "X" + i + "= 0" + j * i + " |");
+				} else {
+					System.out.print(j + "X" + i + "= " + j * i + " |");
+				}
+
+			}
+			System.out.println("");
+		}
+
+	}
+}
