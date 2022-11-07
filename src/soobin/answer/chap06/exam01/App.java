fd1470e86b1c34e37b71d7401064e5f1137f82c7
soobin lee <tnqls7742@gmail.com>
2022-11-07 19:34:27 +0900

    commit test

--- src/soobin/answer/chap06/exam01/App.java
+++ src/soobin/answer/chap06/exam01/App.java
@@ -11,6 +11,8 @@ public class App {
 		//2. Dog 클래스에 생성자를 추가하여 아래의 코드가 동작하게 해봅시다.
 		Dog dog = new Dog("멍멍이", 3);
 		System.out.println(dog.getName() + "는 " + dog.getAge() + "살 입니다.");
+		
+		//merge test
 	}
 
 }
